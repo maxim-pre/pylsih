911483b31702305fda88ceee0d670b5506f32fed
Maxim Prestwich <maximprestwich1@gmail.com>
2023-10-22 15:32:11 +0100

    create basic class for numberNode and BinOpNode

--- lang.py
+++ lang.py
@@ -140,8 +140,27 @@ class Position:
     
     def copy(self):
         return Position(self.idx, self.ln, self.col, self.file_name, self.ftxt)
+    
 
+###################################################
+# NODES
+###################################################
 
+class numberNode: 
+    def __init__(self, tok) -> None:
+        self.tok = tok 
+
+    def __repr__(self) -> str:
+        return f'{self.tok}'
+    
+class binOpNode:
+    def __init__(self, left_node, op_tok, right_node):
+        self.left_node = left_node 
+        self.op_tok = op_tok 
+        self.right_node = right_node 
+    
+    def __repr__(self) -> str:
+        return f'{self.left_node}, {self.op_tok}, {self.right_node}'
             
 ###################################################
 # RUN
