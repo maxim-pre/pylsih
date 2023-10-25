0fe979d324a8b3156549c5f26b10d2cdba425de2
Maxim Prestwich <maximprestwich1@gmail.com>
2023-10-25 12:58:55 +0100

    create RTResult class and Number data_type class

---
+++ src/data_types.py
@@ -0,0 +1,25 @@
+class Number:
+    def __init__(self, value) -> None:
+        self.value = value 
+    
+
+    def set_position(self, pos_start, pos_end):
+        self.pos_start = pos_start 
+        self.pos_end = pos_end
+    
+    def added_to(self, other):
+        if isinstance(other, Number):
+            return Number(self.value + other.value)
+        
+    def subbed_to(self, other):
+        if isinstance(other, Number):
+            return Number(self.value - other.value)
+        
+    def divided_to(self, other):
+        if isinstance(other, Number):
+            return Number(self.value / other.value)
+        
+    def multiplied_to(self, other):
+        if isinstance(other, Number):
+            return Number(self.value * other.value)
+    
--- src/interpreter.py
+++ src/interpreter.py
@@ -1,5 +1,35 @@
+from.data_types import *
+
+class RTResult:
+    def __init__(self):
+        self.value = None 
+        self.error = None 
+    
+    def register(self, res):
+        if res.error: self.error = res.error 
+        return res.value 
+    
+    def failure(self, error):
+        self.error = error 
+        return self
+    
+    def success(self, value):
+        self.value = value 
+        return self
+
 
 
 class interpreter:
     def __init__(self, node):
         self.node = node
+    
+    def visit(self, node):
+        method_name = f'visit_{type(node).__name__}'
+        method = getattr(self, method_name, self.no_visit_method)
+        return method(node)
+
+    def no_visit_method(self, node):
+        raise Exception(f'no visit_{type(node).__name__} method defined')
+    
+    def visit_NumberNode(self, node):
+        return RTResult().success(Number(node.tok.value).set_position(node.pos_start, node.pos_end))
